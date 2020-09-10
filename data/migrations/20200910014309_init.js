cea891d0b20be4e700fc9e9252c1753a66afb972
royeradames <royeraadames@gmail.com>
2020-09-10 12:03:08 -0400

    added pool knex code

--- data/migrations/20200910014309_init.js
+++ data/migrations/20200910014309_init.js
@@ -1,30 +1,30 @@
 
-exports.up = function(knex) {
-  return knex.schema
-  .createTable('clients', tbl => {
-      tbl.increments()
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('clients', tbl => {
+            tbl.increments()
 
-  })
-  .createTable('questions', tbl => {
-      tbl.increments()
+        })
+        .createTable('questions', tbl => {
+            tbl.increments()
 
-  })
-  .createTable('client_questions', tbl => {
-      tbl.increments()
+        })
+        .createTable('client_questions', tbl => {
+            tbl.increments()
 
-      tbl.integer('client_id').unsigned().notNullable().references('clients.id')
-
-      tbl.integer('question_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('questions')
-  })
+            tbl.integer('client_id').unsigned().notNullable().references('clients.id')
+            //foreign key 
+            tbl.integer('question_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('questions')
+        })
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema
-    .dropTableIfExists('client_questions')
-    .dropTableIfExists('questions')
-    .dropTableIfExists('clients')
+        .dropTableIfExists('client_questions')
+        .dropTableIfExists('questions')
+        .dropTableIfExists('clients')
 };

4dce25d28fd8821ca80b8a0e9da375e2d679d034
royeradames <royeraadames@gmail.com>
2020-09-10 12:21:18 -0400

    up to date

--- data/migrations/20200910014309_init.js
+++ data/migrations/20200910014309_init.js
@@ -17,11 +17,13 @@ exports.up = function (knex) {
             tbl.integer('question_id')
                 .unsigned()
                 .notNullable()
-                .references('id')
-                .inTable('questions')
+                .references('id')//links the id of questions table
+                .inTable('questions')//links question table
         })
 };
 
+//think of socks in a foot
+//you have to do the opposite of how you put them on to take them off
 exports.down = function (knex) {
     return knex.schema
         .dropTableIfExists('client_questions')
